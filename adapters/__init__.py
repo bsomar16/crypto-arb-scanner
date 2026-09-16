f17a04a53e7a8c1493f41eaadbf363230bd420d8
bsomar16 <136027087+bsomar16@users.noreply.github.com>
2026-09-16 14:06:23 +0100

    Phase 7: register Bybit SPOT adapter

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,6 +1,7 @@
 """Authenticated SPOT exchange adapters."""
 
 from .binance import BinanceSpotAdapter
+from .bybit import BybitSpotAdapter
 from .okx import OKXSpotAdapter
 
-__all__ = ["BinanceSpotAdapter", "OKXSpotAdapter"]
+__all__ = ["BinanceSpotAdapter", "BybitSpotAdapter", "OKXSpotAdapter"]
